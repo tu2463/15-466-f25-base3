a2d4b20967579600c5ddac9d2fd65b6a5435f912
Jim McCann <ix@tchow.com>
2025-08-22 17:17:02 -0400

    docs and year fixes

--- main.cpp
+++ main.cpp
@@ -67,7 +67,7 @@ int main(int argc, char **argv) {
 
 	//create window:
 	SDL_Window *window = SDL_CreateWindow(
-		"gp23 game3: require sound", //TODO: remember to set a title for your game!
+		"gp25 game3: require sound", //TODO: remember to set a title for your game!
 		1280, 720, //TODO: modify window size if you'd like
 		SDL_WINDOW_OPENGL
 		| SDL_WINDOW_RESIZABLE //uncomment to allow resizing
